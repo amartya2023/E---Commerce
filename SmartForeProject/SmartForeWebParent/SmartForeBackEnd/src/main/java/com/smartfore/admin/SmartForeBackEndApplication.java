f4c8f7bdd8084f140c8260136890919b93011025
Amartya Ghosh <amartyaghosh48@gmail.com>
2024-02-14 10:14:44 +0530

    first commit

---
+++ SmartForeProject/SmartForeWebParent/SmartForeBackEnd/src/main/java/com/smartfore/admin/SmartForeBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.smartfore.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartForeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartForeBackEndApplication.class, args);
+	}
+
+}
---
+++ SmartForeProject/SmartForeWebParent/SmartForeFrontEnd/src/main/java/com/smartfore/site/SmartForeFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.smartfore.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartForeFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartForeFrontEndApplication.class, args);
+	}
+
+}
