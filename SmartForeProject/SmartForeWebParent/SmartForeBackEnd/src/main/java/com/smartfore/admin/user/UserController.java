f1c2cfe3fbabaf7ea92094903579e3bc4f7fd37e
Amartya Ghosh <amartyaghosh48@gmail.com>
2024-02-21 13:23:55 +0530

    New User Interface

--- SmartForeProject/SmartForeWebParent/SmartForeBackEnd/src/main/java/com/smartfore/admin/user/UserController.java
+++ SmartForeProject/SmartForeWebParent/SmartForeBackEnd/src/main/java/com/smartfore/admin/user/UserController.java
@@ -3,10 +3,14 @@ package com.smartfore.admin.user;
 
 import java.util.List;
 
+import javax.management.relation.Role;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.smartfore.common.entity.User;
 
@@ -20,6 +24,30 @@ public class UserController {
 	public String listAll(Model model) {
 		List<User> listUsers = service.listAll();
 		model.addAttribute("listUsers", listUsers);
+		
 		return "users";
+	}
+	
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		List<com.smartfore.common.entity.Role> listRoles = service.listRoles();
+		
+		User user = new User();
+		user.setEnable(true);
+		
+		model.addAttribute("user", user);
+		model.addAttribute("listRoles", listRoles);
+		
+		return "user_form";
+	}
+	
+	@PostMapping("/users/save")
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+		System.out.println(user);
+		service.save(user);
+		
+		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+		
+		return "redirect:/users";
 	}
 }
