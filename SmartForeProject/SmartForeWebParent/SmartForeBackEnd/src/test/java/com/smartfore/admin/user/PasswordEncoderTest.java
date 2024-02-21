4cadef0ce736fdd0be2c687296de937770a4baeb
Amartya Ghosh <amartyaghosh48@gmail.com>
2024-02-21 20:24:03 +0530

    Security Password

---
+++ SmartForeProject/SmartForeWebParent/SmartForeBackEnd/src/test/java/com/smartfore/admin/user/PasswordEncoderTest.java
@@ -0,0 +1,24 @@
+package com.smartfore.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class PasswordEncoderTest {
+	
+	@Test
+	public void testEncodePassword() {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String rawPassword = "amartya2024";
+		String encodedPassword = passwordEncoder.encode(rawPassword);
+		
+		System.out.println(encodedPassword);
+		
+		boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
+		
+		assertThat(matches).isTrue();
+	}
+
+}
