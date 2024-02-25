d8b97ef4db0ed03c84ae7668628d9618f41a8848
Amartya Ghosh <amartyaghosh48@gmail.com>
2024-02-25 12:30:08 +0530

    Pagination function code

--- SmartForeProject/SmartForeWebParent/SmartForeBackEnd/src/main/java/com/smartfore/admin/user/UserRepository.java
+++ SmartForeProject/SmartForeWebParent/SmartForeBackEnd/src/main/java/com/smartfore/admin/user/UserRepository.java
@@ -3,9 +3,15 @@ package com.smartfore.admin.user;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
 import org.springframework.data.repository.query.Param;
 
 import com.smartfore.common.entity.User;
+
+import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+//import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 	@Query("SELECT u FROM User u WHERE u.email = :email")
@@ -16,4 +22,6 @@ public interface UserRepository extends CrudRepository<User, Integer> {
 	@Query("UPDATE User u SET u.enable = ?2 WHERE u.id = ?1")
 	@Modifying
 	public void updateEnableStatus(Integer is, boolean enable);
+	
+	Page<User> findAll(Pageable pageable);
 }
--- SmartForeProject/SmartForeWebParent/SmartForeBackEnd/src/test/java/com/smartfore/admin/user/UserRepositoryTest.java
+++ SmartForeProject/SmartForeWebParent/SmartForeBackEnd/src/test/java/com/smartfore/admin/user/UserRepositoryTest.java
@@ -1,6 +1,9 @@
 package com.smartfore.admin.user;
 
+
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +11,15 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page; // <-- This import conflicts with the other one
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.Rollback;
 
 import com.smartfore.common.entity.Role;
 import com.smartfore.common.entity.User;
 
-@DataJpaTest
+@DataJpaTest(showSql = false)
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @Rollback(false)
 public class UserRepositoryTest {
@@ -117,6 +123,21 @@ public class UserRepositoryTest {
 	public void testEnableUser() {
 		Integer id = 1;
 		repo.updateEnableStatus(id, true);
+	}
+	
+	@Test
+	public void testListFirstPage() {
+		int pageNumber = 0;
+		int pageSize = 4;
+		
+		Pageable pageable = PageRequest.of(pageNumber, pageSize);
+		Page<User> page = repo.findAll(pageable);
+		
+		List<User> listUsers = page.getContent();
+		
+		listUsers.forEach(user -> System.out.println(user));
+		
+		assertThat(listUsers.size()).isEqualTo(pageSize);
 	}
 	
 }
